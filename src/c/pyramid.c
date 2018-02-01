e634160a23a505bc7bf7a9c49f8448623ba6b4dd
Norman Jarvis <norman.jarvis@gmail.com>
2018-01-31 22:15:53 -0700

    Changed the resource to cri from pin and specified if there is no version or a special version then it will always pull the latest for that unpinned cri

--- src/c/pyramid.c
+++ src/c/pyramid.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter number of rows: ");
     scanf("%d",&rows);
 
-    // PUT crpin://coderockit.io/asdf/asdfasdfasdf/v1.1.1/sdfsd
+    // PUT cri://coderockit.io/asdf/asdfasdfasdf/1.1.1/adsf
     for(i=1; i<=rows; ++i, k=0)
     {
         for(space=1; space<=rows-i; ++space)
@@ -22,10 +22,18 @@ int main()
 
         printf("\n");
     }
-    // ENDPUT crpin
+    // END cri
 
-    // GET crpin://coderockit.io/qwer/qwerqwerqwer/v2.3.5/
-    // ENDGET crpin
+    // GET cri://coderockit.io/qwer/qwerqwerqwer/2.3.5/asdfasdf
+    // END cri
+
+    // no version means it will always get the latest
+    // GET cri://coderockit.io/qwer/qwerqwerqwer
+    // END cri
+
+    // ^2.0 means to get the latest version higher than 2.0
+    // GET cri://coderockit.io/qwer/qwerqwerqwer/^2.0
+    // END cri
 
     return 0;
 }
