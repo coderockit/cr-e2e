4cc0dc60756f3b58b00a75d75bc1ac81bb7033cf
Norman Jarvis <norman.jarvis@gmail.com>
2018-02-01 23:06:56 -0700

    Now the URLs are pin URLs as it makes more sense

--- src/c/pyramid.c
+++ src/c/pyramid.c
@@ -3,10 +3,12 @@ int main()
 {
     int i, space, rows, k=0;
 
+    // PUT pin://coderockit.io/asdf/asdfasdfasdf/1.34.7
     printf("Enter number of rows: ");
     scanf("%d",&rows);
+    // ENDPIN
 
-    // PUT cri://coderockit.io/asdf/asdfasdfasdf/1.1.1/adsf
+    // PUT pin://coderockit.io/asdf/asdfasdfasdf/1.1.1/adsf
     for(i=1; i<=rows; ++i, k=0)
     {
         for(space=1; space<=rows-i; ++space)
@@ -22,18 +24,18 @@ int main()
 
         printf("\n");
     }
-    // END cri
+    // ENDPIN
 
-    // GET cri://coderockit.io/qwer/qwerqwerqwer/2.3.5/asdfasdf
-    // END cri
+    // GET pin://coderockit.io/qwer/qwerqwerqwer/2.3.5/asdfasdf
+    // ENDPIN
 
     // no version means it will always get the latest
-    // GET cri://coderockit.io/qwer/qwerqwerqwer
-    // END cri
+    // GET pin://coderockit.io/qwer/qwerqwerqwer
+    // ENDPIN
 
     // ^2.0 means to get the latest version higher than 2.0
-    // GET cri://coderockit.io/qwer/qwerqwerqwer/^2.0
-    // END cri
+    // GET pin://coderockit.io/qwer/qwerqwerqwer/^2.0
+    // ENDPIN
 
     return 0;
 }
