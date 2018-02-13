e0741e7e2d2b67827a2770aa2e6fcaa31e66e21b
Norman Jarvis <norman.jarvis@gmail.com>
2018-02-13 15:48:07 -0700

    Added a few more examples of PUTs that should fail due to permissions

--- src/c/pyramid.c
+++ src/c/pyramid.c
@@ -3,12 +3,12 @@ int main()
 {
     int i, space, rows, k=0;
 
-    // PUTPRIVATE /pin/asdf/asdfasdfasdf/1.34.7
+    // PUTPRIVATE /pin/asdf/asdfasdfasdf::pg/1.34.7
     printf("Enter number of rows: ");
     scanf("%d",&rows);
     // ENDPIN
 
-    // PUT /pin/asdf/asdfasdfasdf/1.1.1/qwer:qwerqwerqwer:2.3.5
+    // PUT /pin/asdf/asdfasdfasdf::yt/1.1.1/qwer:qwerqwerqwer:2.3.5
     for(i=1; i<=rows; ++i, k=0)
     {
         for(space=1; space<=rows-i; ++space)
@@ -26,15 +26,15 @@ int main()
     }
     // ENDPIN
 
-    // GET /pin/qwer/qwerqwerqwer/2.3.5
+    // no version means it will always get the latest
+    // GET /pin/qwer/qwerqwerqwer::openit
     // ENDPIN
 
-    // no version means it will always get the latest
-    // GET /pin/qwer/qwerqwerqwer
+    // GET /pin/qwer/qwerqwerqwer::zipit/2.3.5
     // ENDPIN
 
     // ^2.0 means to get the latest version higher than 2.0
-    // GET /pin/qwer/qwerqwerqwer/^2.0
+    // GET /pin/qwer/qwerqwerqwer::closeit/^2.0
     // ENDPIN
 
     return 0;
